60350aaef31ab64ee79e56a31d4cbcaecdd61551
Nicholas <nicholas.allio@me.com>
2015-11-18 17:34:51 +0100

    added down sampling script for csv

---
+++ down_sampling_1every10.py
@@ -0,0 +1,16 @@
+import csv
+import sys
+
+name_input = sys.argv[1].split(".")[0] # extract input file name
+down_file = name_input + "_down.csv"
+
+with open(sys.argv[1], 'r') as inp, open(down_file, 'w') as outp:
+    writer = csv.writer(outp)
+    reader = csv.reader(inp)
+    count = 0
+    for row in reader:
+        if count % 10 == 0:
+            writer.writerow(row)
+        count +=1
+    outp.close()
+    inp.close()
